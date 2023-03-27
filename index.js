114619a18113506f7c3972f5d3c9ad8a2afc4160
ElvinLee0523 <elvinlee0523@gmail.com>
2023-03-27 15:15:20 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+//let data1 = true /* 1 as num*/
+//let data2 = "1.25" /* str as string*/
+//console.log (data1, typeof data1)
+//console.log (data2, typeof data2)
+
+//let data3 = data1.toString() + data2
+//console.log (data3, typeof data3)
+
+//Array, parseInt, parseFloat, toString()
+//let data = ["one", 2, true]
+//console.log(data[2], typeof data[2])
+
+user = {
+    name: "hehe",
+    faculty: "fkekk",
+    phone: {
+        office: "22222",
+        mobile: "11111"
+    },
+    email:"hehe@utem.edu.my"
+}
+
+console.log(user.phone.office)
+console.log(user.phone.home)
+console.log(user.phone.mobile)
